3243b59c10e365ef18e66e38f10e983f993feffb
TopScrech <sergei_saliukov@icloud.com>
2022-12-04 14:43:46 +0100

    Initial Commit

---
+++ Weather Together/Weather_TogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TogetherApp.swift
+//  Weather Together
+//
+//  Created by Sergei Saliukov on 04.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
